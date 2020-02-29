3ef78b3b125b0952005db3b661cdfdde3f5c8d51
Logan <lmantheyplymouth@gmail.com>
2020-02-29 12:57:41 -0600

    Started some auto mode commenting

---
+++ src/main/java/frc/robot/auto/modes/TrenchRunDeliver.java
@@ -0,0 +1,22 @@
+package frc.robot.auto.modes;
+import frc.robot.auto.AutoModeBase;
+import frc.robot.auto.AutoModeEndedException;
+import frc.robot.auto.actions.DriveStraightActionTime;
+
+import frc.robot.auto.actions.WaitAction;
+
+
+public class TrenchRunDeliver extends AutoModeBase {
+
+	@Override
+	protected void routine() throws AutoModeEndedException {
+
+        //Turn
+        //Auto Allign with Goal
+        //Go Straight
+        //Deliver
+
+		
+	}
+
+}
---
+++ src/main/java/frc/robot/auto/modes/TrenchRunPickupAndDeliver.java
@@ -0,0 +1,24 @@
+package frc.robot.auto.modes;
+import frc.robot.auto.AutoModeBase;
+import frc.robot.auto.AutoModeEndedException;
+import frc.robot.auto.actions.DriveStraightActionTime;
+
+import frc.robot.auto.actions.WaitAction;
+
+
+public class TrenchRunPickupAndDeliver extends AutoModeBase {
+
+	@Override
+	protected void routine() throws AutoModeEndedException {
+
+        //Go Straight
+        //Pickup Balls
+        //Go Backwards
+        //Turn
+        //Allign with goal
+        //Go Straight
+        //Deliver
+
+	}
+
+}
