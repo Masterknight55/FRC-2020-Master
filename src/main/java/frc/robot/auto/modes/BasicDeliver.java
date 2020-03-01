0cae5342128374530508c090c0bdebe26393dd4b
Hannah <roboriotDev1@gmail.com>
2020-02-29 22:15:22 -0600

    auto

---
+++ src/main/java/frc/robot/auto/modes/BasicDeliver.java
@@ -0,0 +1,20 @@
+package frc.robot.auto.modes;
+import frc.robot.auto.AutoModeBase;
+import frc.robot.auto.AutoModeEndedException;
+import frc.robot.auto.actions.DriveStraightActionTime;
+
+import frc.robot.auto.actions.WaitAction;
+
+
+public class BasicDeliver extends AutoModeBase {
+
+	@Override
+	protected void routine() throws AutoModeEndedException {
+
+        //Allign with goal
+        //Go Straight
+        //Deliver
+
+	}
+
+}
