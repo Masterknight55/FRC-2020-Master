d59765f0ca4777db98325dfa8fb5aba861d20d49
RoboRiotDev2 <roboriotDev2@gmail.com>
2020-03-05 17:31:21 -0600

    Stopping Auto Actions

---
+++ src/main/java/frc/robot/auto/actions/StopIntakeAction.java
@@ -0,0 +1,42 @@
+package frc.robot.auto.actions;
+
+import frc.robot.subsystems.Intake;
+
+public class StopIntakeAction implements Action 
+{
+    Intake mIntake;
+
+    public  StopIntakeAction() {
+        mIntake = Intake.getInstance();
+    }
+    
+    @Override
+    public void start() {
+        mIntake.StopIntaking();
+    }
+
+    @Override
+    public void update() {
+        
+
+    }
+
+    public void Stop() {
+
+        
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    public void done() {
+
+    }
+
+    
+
+}
